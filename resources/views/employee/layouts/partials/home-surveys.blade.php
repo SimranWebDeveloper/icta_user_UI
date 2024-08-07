a277b05bd97d0444bb88d66ea4393ea70a2f65a4
malik233222 <131356290+malik233222@users.noreply.github.com>
2024-08-07 11:42:41 +0400

    fixed

--- resources/views/employee/layouts/partials/home-surveys.blade.php
+++ resources/views/employee/layouts/partials/home-surveys.blade.php
@@ -1,121 +1,127 @@
 <style>
-  .swal2-popup {
-      width: 80%;
-  }
+    .swal2-popup {
+        width: 80%;
+    }
 
-  .name-date-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      width: 100%;
-  }   
-  .name-date-wrapper h3, h4 {
-      margin: 0 !important;
-      padding: 0 !important;
-  }
+    .swal2-html-container {
+        overflow-x: hidden !important;
+    }       
 
-  .answers {
-      gap: 1rem;
-      text-align: center;
-      align-items: center;
-  }
+    .name-date-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+    }
 
-  .answers label {
-      width: 100%;
-  }
+    .name-date-wrapper h3,
+    h4 {
+        margin: 0 !important;
+        padding: 0 !important;
+    }
+
+    .answers {
+        gap: 1rem;
+        text-align: center;
+        align-items: center;
+    }
+
+    .answers label {
+        width: 100%;
+    }
 </style>
 <link rel="stylesheet" href="/css/surveys/surveys_cu.css">
 
 <div class="col-lg-4 col-md-6">
-  <div class="card">
-      <div class="card-header">Anketlər</div>
-      <div class="card-body scrollable-content pt-0">
-          <div class="row">
-              @foreach($surveys as $survey)
-              <div class="col-6 mt-4">
-                  <div class="card">
-                      <div class="card-header text-center">{{ $survey->name }}</div>
-                      <div class="card-body">
-                          <div>
-                              <p class="m-0" style="font-weight:bold">Silinmə tarixi:</p>
-                              <p class="m-0">
-                                  {{ \Carbon\Carbon::parse($survey->expired_at)->format('d-m-Y H:i') }} 
-                              </p>
-                          </div>
-                          <div class="mt-3">
-                              <p class="important">Önəmli</p>
-                              <p class="normal">Normal</p>
-                          </div>
-                          <button id="loginButton" class="btn btn-success btn-lg mt-3" data-survey='@json($survey)'>
-                              Cavabla
-                          </button>
-                      </div>
-                  </div>
-              </div>
-              @endforeach
-          </div>
-      </div>
-  </div>
+    <div class="card">
+        <div class="card-header">Anketlər</div>
+        <div class="card-body scrollable-content pt-0">
+            <div class="row">
+                @foreach($surveys as $survey)
+                    <div class="col-6 mt-4">
+                        <div class="card">
+                            <div class="card-header text-center">{{ $survey->name }}</div>
+                            <div class="card-body">
+                                <div>
+                                    <p class="m-0" style="font-weight:bold">Silinmə tarixi:</p>
+                                    <p class="m-0">
+                                        {{ \Carbon\Carbon::parse($survey->expired_at)->format('d-m-Y H:i') }}
+                                    </p>
+                                </div>
+                                <div class="mt-3">
+                                    <p class="important">Önəmli</p>
+                                    <p class="normal">Normal</p>
+                                </div>
+                                <button id="loginButton" class="btn btn-success btn-lg mt-3" data-survey='@json($survey)'>
+                                    Cavabla
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </div>
 
 
 @section('js')
 <script>
-  $(document).ready(function () {
-      $(document).on('click', '#loginButton', function () {
-          const survey = $(this).data('survey');
-          console.log(survey); 
-          Swal.fire({
-              title: survey.name || 'Survey', 
-              html: generateSurveyHtml(survey),
-              showCancelButton: true,
-              confirmButtonText: 'Submit',
-              preConfirm: () => {
-                  const form = document.getElementById('surveyForm');
-                  if (form) {
-                      console.log('Form is being submitted');
-                      form.submit(); 
-                  }
-              }
-          });
-      });
-  });
+    $(document).ready(function () {
+        $(document).on('click', '#loginButton', function () {
+            const survey = $(this).data('survey');
+            console.log(survey);
+            Swal.fire({
+                title: survey.name || 'Survey',
+                html: generateSurveyHtml(survey),
+                showCancelButton: true,
+                confirmButtonText: 'Submit',
+                preConfirm: () => {
+                    const form = document.getElementById('surveyForm');
+                    if (form) {
+                        console.log('Form is being submitted');
+                        form.submit();
+                    }
+                }
+            });
+        });
+    });
 
-  function generateSurveyHtml(survey) {
-      if (!survey || !survey.surveys_questions || !Array.isArray(survey.surveys_questions)) {
-          return '<p>Hələki Heç Bir Sual Yoxdur.</p>'; 
-      }
+    function generateSurveyHtml(survey) {
+        if (!survey || !survey.surveys_questions || !Array.isArray(survey.surveys_questions)) {
+            return '<p>Hələki Heç Bir Sual Yoxdur.</p>';
+        }
 
-      let questionsHtml = '';
-      survey.surveys_questions.forEach((question, index) => {
-          let optionsHtml = '';
-          if (question.input_type === 'radio') {
-              question.answers.forEach(option => {
-                  optionsHtml += `
+        let questionsHtml = '';
+        survey.surveys_questions.forEach((question, index) => {
+            let optionsHtml = '';
+            if (question.input_type === 'radio') {
+                question.answers.forEach(option => {
+                    optionsHtml += `
                       <div class="answers d-flex form-check">
-                          <input class="form-check-input" type="radio" name="question_${index}" id="option_${index}_${option.id}" />
+                          <input class="form-check-input" type="radio" value="" name="question_${index}" id="option_${index}_${option.id}" />
                           <label class="w-100" for="option_${index}_${option.id}">${option.name}</label>
                       </div>
                   `;
-              });
-          } else if (question.input_type === 'checkbox') {
-              question.answers.forEach(option => {
-                  optionsHtml += `
+                });
+            } else if (question.input_type === 'checkbox') {
+                question.answers.forEach(option => {
+                    optionsHtml += `
                       <div class="answers d-flex form-check">
-                          <input class="form-check-input" type="checkbox" name="question_${index}[]" id="option_${index}_${option.id}" />
+                          <input class="form-check-input" type="checkbox" value="" name="question_${index}[]" id="option_${index}_${option.id}" />
                           <label class="w-100" for="option_${index}_${option.id}">${option.name}</label>
                       </div>
                   `;
-              });
-          } else if (question.input_type === 'textarea') {
-              optionsHtml = `
+                });
+            } else if (question.input_type === 'textarea') {
+                optionsHtml = `
                   <div>
-                      <textarea rows="7" cols="10" class="form-control" name="question_${index}"></textarea>
+                      <textarea rows="7" cols="10" class="form-control" value="" name="question_${index}"></textarea>
                   </div>
               `;
-          }
+            }
 
-          questionsHtml += `
+            questionsHtml += `
               <div class="col-md-6 col-sm-12 mb-4">
                   <div class="card">
                       <div class="card-header d-flex justify-content-center">
@@ -128,9 +134,9 @@
                   </div>
               </div>
           `;
-      });
+        });
 
-      return `
+        return `
           <div class="pt-4 custom-container bg-white">
               <div class="row">
                   <div class="col-12">
@@ -158,8 +164,7 @@
               </div>
           </div>
       `;
-  }
+    }
 </script>
 
 @endsection
-
