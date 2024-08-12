2792cde4b3442aac262bbdd21dce31d9e610095a
SimranWebDeveloper <112202814+SimranWebDeveloper@users.noreply.github.com>
2024-08-13 00:42:21 +0400

    Update survey.js

--- public/js/employee/survey.js
+++ public/js/employee/survey.js
@@ -17,39 +17,116 @@ $(document).ready(function () {
         openNextSurvey();
     }
 
+        // anket Yenilendi
+        $(".allSurveysButton").on("click", function () {
+            const survey = $(this).data("survey");
+            const surveyId = $(this).data("survey-id");
+            // Cavablari gör
+                $.ajax({
+                    url: `/employee/survey/answers/${surveyId}`,
+                    method: 'GET',
+                    success: function (response) {
+                        const survey = surveys.find(s => s.id === surveyId);
+                        if (survey) {
+                            showUserAnswersPopup(response, survey);
+                            showSurveyPopup(survey, survey.priority === 0);
+                        }
+                    },
+                    error: function (error) {
+                        console.error("Failed to fetch user answers:", error);
+                    }
+                });
+    
+            // Cavabla
+            // showSurveyPopup(survey, survey.priority === 0);
+        });
+
+        
+
+            // Cavablari gör
+    function showUserAnswersPopup(answers, survey) {
+        let answersHtml = '';
+    
+        survey.surveys_questions.forEach((question, index) => {
+            const questionId = question.id;
+            const questionType = question.input_type; // Determine the question type (checkbox, radio, textarea)
+    
+            // Get the list of user's answers for this question
+            const answerList = answers[questionId] || []; // Adjust based on the response structure
+    
+            answersHtml += `<div class="col-xl-6 col-12">                        
+                <div class="card mb-4">
+                    <div class="card-header w-100 d-flex justify-content-center align-items-center">
+                        <h3 class="m-0">${index + 1}.</h3>
+                        <h3 class="m-0">${question.question}</h3>
+                    </div>
+                    <div class="card-body">`;
+    
+            if (questionType === 'textarea') {
+                // Display the textarea with the user's answer
+                const textareaAnswer = answerList[0] ? answerList[0].answer : ''; // Adjust based on response structure
+            answersHtml += `<textarea disabled  rows="10" style='box-sizing:border-box; width: 100%;resize: "none" '>${textareaAnswer}</textarea>`;
+            } else {
+                // Display the options with user answers marked as checked
+                answersHtml += `<ul class="list-group-custom">`;
+                question.answers.forEach((option) => {
+                    // Determine if this option should be checked
+                    const isChecked = answerList.some(answer => answer.answer === option.name);
+    
+                    answersHtml += `<li class="d-flex my-3 align-items-center w-100 justify-content-between">
+                        <div class="d-flex align-items-center justify-content-between  w-100 py-2">
+                            <div class="d-flex align-items-center justify-content-center">                                                
+                                <input type="${questionType}" disabled ${isChecked ? 'checked' : ''} class="rounded" style="width: 20px; height: 20px" />
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center  w-100 pl-3">
+                                <label class="text-justify">
+                                    ${option.name}
+                                </label>
+                            </div>
+                        </div>
+                    </li>`;
+                });
+                answersHtml += `</ul>`;
+            }
+    
+            answersHtml += `</div>
+                </div>
+            </div>`;
+        });
+    
+        Swal.fire({
+            title: "User Answers",
+            html: `
+                <div class="row mb-4 w-100">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    ${answersHtml}
+                                    
+                                    <!-- Please add here this code -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`,
+            showCancelButton: false,
+            confirmButtonText: "Ok",
+        });
+    }
+
     // Cavablari gör
     $(".showSurveyButton").on("click", function () {
         const surveyId = $(this).data("survey-id");
         fetchUserAnswers(surveyId);
+
     });
     // Cavabla
     $(".surveyButton").on("click", function () {
         const survey = $(this).data("survey");
         showSurveyPopup(survey, survey.priority === 0);
     });
-    // anket Yenilendi
-    $(".allSurveysButton").on("click", function () {
-        showNewAnswersPopup();
-        const survey = $(this).data("survey");
-        const surveyId = $(this).data("survey-id");
-        // Cavablari gör
-            // $.ajax({
-            //     url: `/employee/survey/answers/${surveyId}`,
-            //     method: 'GET',
-            //     success: function (response) {
-            //         const survey = surveys.find(s => s.id === surveyId);
-            //         if (survey) {
-            //             showNewAnswersPopup(response, survey);
-            //         }
-            //     },
-            //     error: function (error) {
-            //         console.error("Failed to fetch user answers:", error);
-            //     }
-            // });
 
-        // Cavabla
-        // showSurveyPopup(survey, survey.priority === 0);
-    });
 
 
 
@@ -336,8 +413,6 @@ $(document).ready(function () {
         });
     }
     
-    
-
     // Cavablari gör
     function fetchUserAnswers(surveyId) {
         $.ajax({
@@ -355,6 +430,8 @@ $(document).ready(function () {
             }
         });
     }
+    
+
 
     // Cavablari gör
     function showUserAnswersPopup(answers, survey) {
